a99d02be9aee1a0ff77c4eb19d2ffbee08fa64d3
Jozev <maga.bid09@gmail.com>
2024-04-07 16:33:33 +0300

    new commit

---
+++ ООАиД_ЛБ1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ООАиД_ЛБ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Hello, World!");
+            Console.ReadLine();
+        }
+        static async void Print(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                Thread.Sleep(60);
+            }
+        }
+        static void Beep(char input)
+        {
+            int chas = Convert.ToInt32(input);
+            Console.Beep(input, chas);
+        }
+    }
+}
